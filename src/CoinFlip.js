358f1eb636333340cb63ce0c96f57d5add7c152f
Lukazovic <lucas10.vieira.lv@gmail.com>
2020-03-31 16:17:38 -0300

    Update

--- src/CoinFlip.js
+++ src/CoinFlip.js
@@ -3,28 +3,48 @@ import React, { Component } from 'react'
 import './CoinFlip.css'
 
 import Coin from './Coin'
+import {choice} from './Helpers'
 
 class CoinFlip extends Component{
   static defaultProps = {
-    options: ['heads', 'tails'],
+    coins: [
+      { side: 'heads', imgSrc: 'https://upload.wikimedia.org/wikipedia/commons/c/cd/S_Half_Dollar_Obverse_2016.jpg'},
+      { side: 'tails', imgSrc: 'http://www.pcgscoinfacts.com/UserImages/71009269r.jpg'}
+    ]
   }
   constructor(props){
     super(props)
-    this.state = {totalCount: 0, headCount: 0, tailCount: 0, option: 'heads'}
+    this.state = {
+      totalCount: 0, 
+      headCount: 0, 
+      tailCount: 0, 
+      currentCoin: 'heads'}
     this.handleClick = this.handleClick.bind(this)
   }
-  generateRandNum(){
-    return Math.floor(Math.random() * 2)
+  flipCoin(){
+    const newCoin = choice(this.props.coins)
+    this.setState(oldState => {
+      let newState = {
+        ...oldState,
+        currentCoin: newCoin,
+        totalCount: oldState.totalCount + 1
+      }
+      if (newCoin.side === 'heads'){
+        newState.headCount += 1
+      } else {
+        newState.tailCount += 1
+      }
+      return newState
+    })
   }
-  handleClick(){
-    console.log(this.props.options[this.generateRandNum()])
-    this.setState = {option: this.props.options[this.generateRandNum()]}
+  handleClick(event){
+    this.flipCoin()
   }
   render(){
     return (
       <div className="CoinFlip">
         <h1>Coin Flip Game</h1>
-        <Coin coin={this.state.option} className="CoinFlip-coin"/>
+        {/* <Coin coin={this.state.currentCoin} className="CoinFlip-coin"/> */}
         <p>Out of {this.state.totalCount}, there have been {this.state.headCount} heads and {this.state.tailCount} tails </p>
         <button onClick={this.handleClick} className="CoinFlip-button">Flip</button>
       </div>
